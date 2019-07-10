7d0fd6b6b6b69a78a77f8991806458f0d16fd59c
Carlo <carlomjavier@gmail.com>
2019-07-10 01:02:38 +0000

    complete exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,19 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+surrey = Store.create! :name => 'Guildford', :annual_revenue => 224000, :mens_apparel => false, :womens_apparel => true
+whistler = Store.create! :name => 'Blackcomb', :annual_revenue => 1900000, :womens_apparel => false, :mens_apparel => true
+railtown = Store.create! :name => 'Wings and Horns', :annual_revenue => 430000, :mens_apparel => true, :womens_apparel => true
+
+@mens_stores = Store.where(mens_apparel: true, womens_apparel: false).find_each do |store|
+    puts store.name
+    puts store.annual_revenue
+end
+
+@womens_under_aMilli = Store.wheere("womens_apparel = true && annual_revenue < 1000000").find_each do |store|
+    puts store.nameputs store.and
+end
+
+
+
