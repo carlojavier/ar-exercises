0e34f2370d3b0e693ea8d8a0d9ae54d86b46e0a1
Carlo <carlomjavier@gmail.com>
2019-07-10 00:08:43 +0000

    load store 1 and 2, change name of store 1

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,15 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id:1)
+@store2 = Store.find_by(id:2)
+
+puts @store1.name
+puts @store2.name
+
+@store1.name = 'Crystal Mall'
+@store1.save
+puts @store1.name
+
+
--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,6 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find(3)
+
+gastown.destroy!
